c24594b5fe4bdc319b4b6d66b6b92ae3c81a2215
Manish Rawat <132916222+Manishrwt15@users.noreply.github.com>
2025-04-13 20:16:27 +0530

    first commit

--- app/api/uploadthing/route.ts
+++ app/api/uploadthing/route.ts
@@ -1,5 +1,4 @@
-// Resource: https://docs.uploadthing.com/nextjs/appdir#create-a-nextjs-api-route-using-the-filerouter
-// Copy paste (be careful with imports)
+// This file contains the route handler for Uploadthing
 
 import { createNextRouteHandler } from "uploadthing/next";
 
